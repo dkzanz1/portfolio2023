dd64f1bdb3301c030b16c20f928be91d90397160
dkzanz1 <paul.zolik@sky.com>
2023-01-28 10:31:04 +0000

    adding files.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function nav() {
+    return <>
+        <header>
+            <h2 className="logo">The Zogg creation company</h2>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWAf7Bm4tyV3YabASUaNsdhfsZuAkuNu3iPQ&usqp=CAU" alt="logo" />
+
+            <ul >
+
+                <li ><a href="*" className="prime">Contact</a></li>
+                <li ><a href="*" className="prime">Info</a></li>
+                <li ><a href="*" className="prime">About</a></li>
+            </ul>
+            <div className="banner">
+                <h1 className="name">Paul Xolik</h1>
+                <p>Web Developer<strong>-></strong> I build webpages </p>
+            </div>
+        </header>
+        <body>
+            <h1>body begins here</h1>
+        </body>
+    </>
+}
+export default nav;
