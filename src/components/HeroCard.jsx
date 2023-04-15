25f4d8a025d0e4ffdc26626168772a22ceeff882
Dkzanz1 <paul.zolik@sky.com>
2023-04-15 18:30:44 +0100

    update 2

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -12,7 +12,7 @@ function Card() {
                 <img src={img} alt="Avatar" className="circle-img" />
                      <h1 className="name"><span>Hi,<br/></span><span>I'm Paul</span></h1>
                      <div className="hero-blurb">
-                    <h2>Web Developer<br /> based in Christchurch,<br/>Dorset</h2>
+                    <h2>Web Developer<br /> who lives by the Sea, <br/>based in Christchurch,<br/>Dorset</h2>
                     <p className="HeroInfo">I build Crafted Bespoke Webpages<br/>
                     using both<strong> Front end and Backend</strong></p>
                     </div>
