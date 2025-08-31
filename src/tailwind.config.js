945cd921af67f3208badb4519fb31cbcac00c30e
paulzolik <paul.zolik@sky.com>
2025-08-31 12:30:16 +0100

    adding tailwind.css config file

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class', // Enables dark mode via a 'dark' class
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
