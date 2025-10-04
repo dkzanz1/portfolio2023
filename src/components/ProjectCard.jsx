69df168e423f4a708b658c04ca6a9f07d0350752
paulzolik <paul.zolik@sky.com>
2025-10-04 15:12:22 +0100

    updating styling to flex box from postioninig

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,35 +1,4 @@
-// import React from "react";
-// import styles from "./ProjectCard.module.css";
 
-//  const ProjectCard = ({id, title, img, url, description }) => {
-//   console.log("ProjectCard props:", { id, title, img, url, description }); // Added this line
- 
-// return (
-//           <div className={styles.projectcardContainer}>
-//             <div className={styles.projectcardItem}>    
-//               <img className={styles.projectcardItemImg} 
-//               src={img} 
-//               alt="avitar of author"/>
-//                 <h2 className={styles.projectcardItemTitle}>{title}</h2>
-//                 <p className={styles.projectcardId}>{id}</p>
-//                 <a
-//                  className={styles.projectcardLink} 
-//                 href={url} 
-//                 target='_blank' 
-//                 rel="noopener noreferrer"
-//                 >
-//                 Visit Project
-//                 </a>
-//                 <p className={styles.projectcardItemInfo}>{description}</p>
-//             </div>
-//           </div>
-        
-//     );
-// }
-
-// console.log(ProjectCard);
-// export default ProjectCard;
-// ProjectCard.js
 import React from "react";
 import styles from "./ProjectCard.module.css";
 import PropTypes from "prop-types"; // Import PropTypes
@@ -40,8 +9,10 @@ const ProjectCard = ({ id, title, img, url, description }) => {
   return (
     <div className={styles.projectcardContainer}>
       <div className={styles.projectcardItem}>
+      <div className={styles.imageWrapper}>
         <img className={styles.projectcardItemImg} src={img} alt="project" />
         <h2 className={styles.projectcardItemTitle}>{title}</h2>
+        </div>
         <p className={styles.projectcardId}>{id}</p>
         <a
           className={styles.projectcardLink}
