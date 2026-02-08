c78cd3fc3adb87160528c8c7bf8fc01b9a9820e3
paulzolik <paul.zolik@sky.com>
2026-02-08 15:19:28 +0000

    feat(herocard): implement fluid responsive breakout and object-fit coverage

--- src/components/WaterVideo/WaterVideo.jsx
+++ src/components/WaterVideo/WaterVideo.jsx
@@ -6,7 +6,21 @@ const WaterVideo = () => {
   return (
     <div className={styles.videobgWrapper}>
       <div className={styles.waterVideo}>
-        <video src={videobg} type="video/mp4" autoPlay loop muted />
+        <video
+          src={videobg}
+          type="video/mp4"
+          autoPlay
+          loop
+          muted
+          style={{
+            position: "absolute" /* Take it out of the document flow */,
+            top: 0,
+            left: 0,
+            objectFit: "cover",
+            width: "100%",
+            height: "100%",
+          }}
+        />
       </div>
     </div>
   );
